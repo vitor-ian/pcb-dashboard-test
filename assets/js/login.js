fe2e45c52e60db5d599babc3c7701b7315a0dd48
Vitor Ian <goncalvesvitorian@gmail.com>
2022-06-02 17:51:51 -0300

    add new user

--- assets/js/login.js
+++ assets/js/login.js
@@ -7,7 +7,7 @@ localStorage.clear();
 let users = [
     {id: 1, nick: 'Nairo', name: 'nairo', senha: "123", cargo: "Administrador", medalha: "1", data: "01/06/2022"},
     {id: 2, nick: 'Baiano', name: 'baiano', senha: "123", cargo: "Membro", medalha: "1", data: "02/06/2022"},
-    {id: 2, nick: 'DominouSX', name: 'xitao', senha: "123", cargo: "Moderador", medalha: "1", data: "02/06/2022"},
+    {id: 3, nick: 'DominouSX', name: 'xitao', senha: "123", cargo: "Moderador", medalha: "1", data: "02/06/2022"},
 ];
 
 function login(){
